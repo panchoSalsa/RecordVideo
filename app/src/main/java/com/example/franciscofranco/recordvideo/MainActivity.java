b86cc58eb074c77d73cf662959d54ff83dadd7be
Franco <frfranco@uci.edu>
2016-08-02 15:07:33 -0700

    record video, save and display it in main activity

--- app/src/main/java/com/example/franciscofranco/recordvideo/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/recordvideo/MainActivity.java
@@ -1,15 +1,78 @@
 package com.example.franciscofranco.recordvideo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_TAKE_VIDEO = 1;
+    public static final String AUTHORITY = "com.example.franciscofranco.recordvideo";
+    private VideoView videoView;
+    private Uri videoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.videoView);
     }
 
+    public void recordVideo(View v) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            File videoFile = createVideoFile();
+            if (videoFile != null) {
+                videoUri = FileProvider.getUriForFile(this, AUTHORITY,videoFile);
+
+                if (videoUri == null)
+                    return;
+
+                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+//                Log.d("FRANCO_DEBUG", videoFile.toString());
+//                Log.d("FRANCO_DEBUG", videoUri.toString());
+                startActivityForResult(takeVideoIntent, REQUEST_TAKE_VIDEO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_VIDEO && resultCode == RESULT_OK) {
+            videoView.setVideoURI(videoUri);
+            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    videoView.start();
+                }
+            });
+        }
+    }
+
+    public File createVideoFile() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String videoFileName = "MP4_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_MOVIES);
+        File videoFile = null;
+        try {
+            videoFile = File.createTempFile(videoFileName, ".mp4", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return videoFile;
+    }
 
 }
